4eb5fd25a6696fc461a0d4d07a30c09000ae594e
car8411 <car8411@naver.com>
2024-09-11 18:15:55 +0900

    gitignore changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [responseMessage, setResponseMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:3001/save-data', { id: '1', name });
+      setResponseMessage(response.data.message);
+    } catch (error) {
+      setResponseMessage('Error saving data');
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter Name"
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{responseMessage}</p>
       </header>
     </div>
   );
