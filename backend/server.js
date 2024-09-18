29b3a9243a151620928a0e608e7ca7a4ff7457fd
car8411 <car8411@naver.com>
2024-09-18 16:40:20 +0900

    /api

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(express.static('public'));  // public 폴더에서 정적 파일 제공
 
 // 루트 경로에 대한 요청 처리
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Welcome to the home page!');
 });
 
