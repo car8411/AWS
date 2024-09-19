08756530eb89f76ba06baac7fcc25261e9a6e681
car8411 <car8411@naver.com>
2024-09-20 00:47:46 +0900

    s

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,8 @@ function App() {
             <Route path="/about" element={<AboutPage />} />
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/product" element={<ProductDetail />} />
+            <h1>sssssssss
+            </h1>
           </Routes>
         </div>
         <Footer />
