47370b55368ed39ea167d75535751b2bf001d6f8
car8411 <car8411@naver.com>
2024-09-20 01:08:08 +0900

    S

--- frontend/src/pages/ProductDetail.js
+++ frontend/src/pages/ProductDetail.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import PayPalCheckout from '../components/PayPalCheckout';
 
 const ProductDetail = () => {
   return (
     <div>
       <h2>Product Detail</h2>
       <p>여기에 제품 세부 정보를 추가할 수 있습니다.</p>
-
+        <PayPalCheckout/>
     </div>
   );
 };
