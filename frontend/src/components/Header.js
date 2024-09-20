cdba694770b0cd78ba5929482e0921a7aaba9639
car8411 <car8411@naver.com>
2024-09-20 16:03:17 +0900

    D

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,19 @@
 // src/components/Header.js
-import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';  // CSS 파일을 가져옵니다.
+import React, { useState } from 'react';
+
 
 const Header = () => {
+
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  const handleDropdownToggle = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
+
+
   return (
     <header className="App-header">
       <nav className="navbar">
@@ -13,7 +23,19 @@ const Header = () => {
           <li><Link to="/contact">Contact</Link></li>
           <li><Link to="/product">Products</Link></li>
           <li><Link to="/upload">Upload</Link></li>
-          <li><Link to="/locations/:locationId">Upload</Link></li>
+          <li className="dropdown">
+            <button onClick={handleDropdownToggle} className="dropdown-btn">
+              Locations
+            </button>
+            {isDropdownOpen && (
+              <ul className="dropdown-content">
+                <li><Link to="/locations/paris">Paris</Link></li>
+                <li><Link to="/locations/new-york">New York</Link></li>
+                <li><Link to="/locations/tokyo">Tokyo</Link></li>
+                <li><Link to="/locations/sydney">Sydney</Link></li>
+              </ul>
+            )}
+          </li>
         </ul>
       </nav>
     </header>
