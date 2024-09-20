3aacb3950034c65ae5ab92213f3c72015dc4cd70
car8411 <car8411@naver.com>
2024-09-20 13:18:16 +0900

    header styling

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,18 @@
 // src/components/Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';  // CSS 파일을 가져옵니다.
 
 const Header = () => {
   return (
     <header className="App-header">
-      <nav>
-        <ul>
+      <nav className="navbar">
+        <ul className="nav-links">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
-          <li><Link to="/product">product</Link></li>
-          <li><Link to="/upload">upload</Link></li>
+          <li><Link to="/product">Products</Link></li>
+          <li><Link to="/upload">Upload</Link></li>
         </ul>
       </nav>
     </header>
