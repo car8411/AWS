697adb591296f739db171ce6ca9acdf6906241a7
car84 <car8411@naver.com>
2024-09-29 23:39:44 +0900

    d

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,6 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <h1>This is being made by me and GPT.</h1>
       <p>© 2024 Your Company. All rights reserved.</p>
     </footer>
   );
